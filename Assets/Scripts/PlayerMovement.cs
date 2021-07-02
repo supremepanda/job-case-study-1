bed6deb1fa7da05dc0d9c2405d446dc129c7b7d2
Furkan Baldır <36567819+supremepanda@users.noreply.github.com>
2021-07-03 00:13:02 +0300

    Add navmesh agent system for banana.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
