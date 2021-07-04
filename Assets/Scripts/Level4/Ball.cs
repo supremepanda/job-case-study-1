ec9abd3c0ae2816adfe2c34cf72729d09e9acd15
supremepanda <furkanbaldir13@gmail.com>
2021-07-04 22:23:27 +0300

    Create level4 paddle and ball mechanic.

---
+++ Assets/Scripts/Level4/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Level4
+{
+    public class Ball : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        
+        private Rigidbody _rigidbody;
+
+        private static float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth) 
+        {
+            return (ballPos.x - racketPos.x) / racketWidth;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Paddle"))
+            {
+                float x = HitFactor(transform.position, other.transform.position, other.collider.bounds.size.x);
+                Vector3 direction = new Vector3(x, 0, 1).normalized;
+                _rigidbody.velocity = direction * speed;
+            }
+        }
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.velocity = new Vector3(0, 0, -1) * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Level4/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Level4
+{
+    public class Paddle : MonoBehaviour
+    {
+        private const float PaddleRange = 2.63f;
+
+        private void Update()
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = 10;
+            float wordlPos = Mathf.Clamp(Camera.main.ScreenToWorldPoint(pos).x, -2.63f, 2.63f);
+
+            transform.position = new Vector3(wordlPos, 0f, -5f);
+        }
+    }
+}
